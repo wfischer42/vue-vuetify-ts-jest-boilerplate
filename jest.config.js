5e59b60cf5e53a3bf3fb5e0547f795dedf841ce0
William Fischer <william.fischer42@gmail.com>
2019-06-07 16:56:34 -0600

    Set up identity object proxy for css modules.

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,8 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!(vuetify/))'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    // '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
   },
